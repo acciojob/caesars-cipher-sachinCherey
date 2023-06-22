3a8d69cdba6a8c856bbbcf00496c9c971eb329bc
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-06-22 15:27:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -47,12 +47,16 @@ function rot13(encodedStr) {
 	 }
 	  if(flag==0)decodedArr.push(encodedStr[i]);
   }
-  return decodedArr;
+  let s="";
+  for(let i=0;i<decodedArr.length;i++){
+      s=s+decodedArr[i];
+  }
+  return s;
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(rot13("SERR YBIR? NPPVBWBO"));
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
 window.rot13 = rot13;
